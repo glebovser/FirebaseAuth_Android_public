0445afb77abdbfd27a27202e85ac9967c1293ab9
Konstantin <kok@marketlogicsoftware.com>
2017-08-21 02:52:04 +0200

    commit1

--- app/src/main/java/com/google/firebase/quickstart/auth/ChooserActivity.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/ChooserActivity.java
@@ -44,24 +44,13 @@ public class ChooserActivity extends AppCompatActivity implements AdapterView.On
 
     private static final Class[] CLASSES = new Class[]{
             GoogleSignInActivity.class,
-            FacebookLoginActivity.class,
-            TwitterLoginActivity.class,
             EmailPasswordActivity.class,
-            PhoneAuthActivity.class,
-            AnonymousAuthActivity.class,
-            FirebaseUIActivity.class,
-            CustomAuthActivity.class
+
     };
 
     private static final int[] DESCRIPTION_IDS = new int[] {
             R.string.desc_google_sign_in,
-            R.string.desc_facebook_login,
-            R.string.desc_twitter_login,
             R.string.desc_emailpassword,
-            R.string.desc_phone_auth,
-            R.string.desc_anonymous_auth,
-            R.string.desc_firebase_ui,
-            R.string.desc_custom_auth,
     };
 
     @Override
